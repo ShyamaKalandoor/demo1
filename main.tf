f7f608c9700125438627987aabcfd8554851582b
shyama <skalandoor@paloaltonetworks.com>
2023-02-27 10:42:13 -0800

    main.tf commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+# Define AWS provider
+provider "aws" {
+  region = "us-east-1" # Update with desired region
+}
+
+# Define variables
+variable "instance_name" {
+  default = "my-ec2-instance" # Update with desired instance name
+}
+
+# Create EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID
+  instance_type = "t2.micro"
+  key_name      = "my-keypair" # Update with desired key pair name
+  tags = {
+    Name = var.instance_name
+  }
+
+  # Open port 22 to all traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
